c28ea3f275249f1e3ce49a522586ec3add758e12
Umair Mehmood <126847110+UMAIRJM@users.noreply.github.com>
2024-06-29 20:39:00 +0500

    Today last commit
    Data fetching and searching property completed only design remain

--- movienet/src/components/mainComponent.js
+++ movienet/src/components/mainComponent.js
@@ -1,40 +1,79 @@
 
 import React, { useState, useEffect } from 'react';
 
-
+import ActionAreaCard from './MUICard';
 
 
 
 
 function MainComponent(){
     const [fetchedData,setFetchedData] = useState([]);
-    const API_URL = 'http://www.omdbapi.com/?apikey=707f112d';
-    
-
+        const API_URL = 'http://www.omdbapi.com/?apikey=707f112d';
+        
+    const [searched,setSearched] = useState('')
+    const[error,setError] = useState(false)
 
     async function movieFetching(title){
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json;
-        console.log(data)
-        setFetchedData(data)
+        if(title === '' || title === undefined){
+            setError(true)
+        }
+        else{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        .then((data1)=>{
+            return data1.json()
+        })
+        .then((value2=>{
+            console.log(value2.Search)
+            const newData = value2.Search
+            console.log("I am new data"  + newData[0].Title)
+            newData.forEach(element => {
+                console.log(element.Title)
+            });
+            
+            setFetchedData(newData)
+
+          
+        }))
+    }
 
 
     }
+    function buttonHandler(){
+        movieFetching(searched)
+    }
+
+    function TextHandler(e){
+        setSearched(e.target.value)
+
+    }
     useEffect( ()=>{
-        movieFetching('spiderman');
+        movieFetching('venom');
         
     
-    })
+    },[])
+
+
+
     return(
         <div>
-            {fetchedData.map(
-                item=>(
+            
+            <div>
+                <input type='text' placeholder='Enter Movie name' onChange={TextHandler}/>
+                <button type='buttn' onClick={buttonHandler}> Search</button>
+            </div>
+            {fetchedData.map(item=>(
                     <div>
-                    <p>{item.imdbID}</p>
-                    <p>{item.movie}</p>
+                    {/* <p>{item.Title}</p>
+                    <p>{item.Year}</p>
+                    <img src={item.Poster} alt='moviePoster'></img> */}
+                   <ActionAreaCard title={item.Title} image={item.Poster} year={item.Year}  type={item.Type}/>
                     </div>  
                 )
             )}
+            
+            <p>hello</p>
+
+
         </div>
     )
 }
